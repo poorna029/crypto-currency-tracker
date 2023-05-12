2190d858908eebaa3eaa5c9740a8832cae67b8c5
Poorna <poorna029@gmail.com>
2023-05-12 10:00:34 +0000

    all 14/14 test cases passed

--- src/components/CryptocurrencyItem/index.js
+++ src/components/CryptocurrencyItem/index.js
@@ -4,15 +4,12 @@ import './index.css'
 class CryptocurrencyItem extends Component {
   render() {
     const {item} = this.props
-    // console.log(item)
     if (item === undefined) {
       return null
     }
     const {usdValue, euroValue, currencyName, currencyLogo, id} = item
     console.log(usdValue, euroValue, currencyName, currencyLogo)
-    // console.log(item.usdValue)
-    // console.log('k', this.props)
-    // console.log('l', item)
+
     return (
       <li className="CryptocurrencyItem">
         <div className="logo-name">
